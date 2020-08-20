439fb449cb958e914b8a7e36298d12901f6f5f86
Pramesh Trivedi <ptrivedi@FederalLogic.com>
2020-08-20 08:30:13 -0400

    Add Kiwi
    Signed-off-by: Pramesh Trivedi <ptrivedi@FederalLogic.com>

---
+++ src/com/pluralsight/fruit/Kiwi.java
@@ -0,0 +1,17 @@
+package com.pluralsight.fruit;
+
+public class Kiwi implements IFruit{
+
+
+    @Override
+    public String getName() {
+        return "Kiwi";
+    }
+
+    @Override
+    public int getRollMultiplier() {
+        return 0;
+    }
+}
+
+
